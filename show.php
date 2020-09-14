d96ee827e8f454f35038be4060f11a5a9b577b0e
tmusenko <66254894+tmusenko@users.noreply.github.com>
2020-09-14 12:08:48 +0100

    Add files via upload

---
+++ show.php
@@ -0,0 +1,29 @@
+<?php
+
+	$executionStartTime = microtime(true) / 1000;
+
+	$url='http://api.geonames.org/timezoneJSON?formatted=true&lat=' . $_REQUEST['lat'] . '&lng=10.2&username=flightltd&style=full';
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL,$url);
+
+	$result=curl_exec($ch);
+
+	curl_close($ch);
+	$decode = json_decode($result,true);
+	
+	
+	$output['status']['code'] = "200";
+	$output['status']['result'] = "ok";
+	$output['status']['description'] = "mission saved";
+	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+	$output['data'] = $decode;
+	
+	header('Content-Type: application/json; charset=UTF-8');
+	echo json_encode($output);
+
+?>
+
+
